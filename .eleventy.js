a6b49ada97c62175182024e1e2da87363cb0702f
Lukas Kluge <lukas.kluge@protonmail.com>
2021-12-20 00:14:06 +0100

    add fonts and change 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/fonts/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/fonts/");
+  eleventyConfig.addWatchTarget("./src/images/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
       input: "src",
-      output: "public",
+      output: "_site",
     },
   };
 };
